12ea83608aa785175f68838fa99eff34b433236d
fred3m <fred.moolekamp@gmail.com>
2024-11-29 12:01:11 -0800

    Clean up initialization module

--- python/lsst/scarlet/lite/initialization.py
+++ python/lsst/scarlet/lite/initialization.py
@@ -20,7 +20,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import logging
-from abc import ABC, abstractmethod
+from deprecated.sphinx import deprecated
 from typing import Sequence, cast
 
 import numpy as np
@@ -39,8 +39,9 @@ logger = logging.getLogger("scarlet.lite.initialization")
 
 def trim_morphology(
     morph: np.ndarray,
-    bg_thresh: float = 0,
+    thresh: float = 0,
     padding: int = 5,
+    bg_thresh: float | None = None,
 ) -> tuple[np.ndarray, Box]:
     """Trim the morphology up to pixels above a threshold
 
@@ -48,8 +49,10 @@ def trim_morphology(
     ----------
     morph:
         The morphology to be trimmed.
-    bg_thresh:
+    thresh:
         The morphology is trimmed to pixels above the threshold.
+    bg_thresh:
+        Deprecated in favor of `thresh`.
     padding:
         The amount to pad each side to allow the source to grow.
 
@@ -60,12 +63,19 @@ def trim_morphology(
     box:
         The box that contains the morphology.
     """
+    # Temporarily support bg_thresh
+    if bg_thresh is not None:
+        logger.warning(
+            "bg_thresh is deprecated and will be removed in v28.0. "
+            "Use thresh instead."
+        )
+        thresh = bg_thresh
+
     # trim morph to pixels above threshold
-    mask = morph > bg_thresh
+    mask = morph > thresh
     morph[~mask] = 0
     bbox = Box.from_data(morph, threshold=0).grow(padding)
     return morph, bbox
-
 
 def init_monotonic_morph(
     detect: np.ndarray,
@@ -75,6 +85,9 @@ def init_monotonic_morph(
     normalize: bool = True,
     monotonicity: Monotonicity | None = None,
     thresh: float = 0,
+    max_iter: int = 0,
+    center_radius: int = 2,
+    variance_factor: float = 1
 ) -> tuple[Box, np.ndarray | None]:
     """Initialize a morphology for a monotonic source
 
@@ -98,8 +111,20 @@ def init_monotonic_morph(
         monotonic pixels, otherwise the monotonicity operator is used to
         project the morphology to a monotonic solution.
     thresh:
-        The threshold (fraction above the background) to use for trimming the
+        The threshold to use for trimming the
         morphology.
+    max_iter:
+        The maximum number of iterations to use in the monotonicity operator.
+        Only used if `monotonicity` is `None`.
+    center_radius:
+        The amount that the center can be shifted to a local maximum.
+        Only used if `monotonicity` is `None`.
+    variance_factor:
+        The average variance in the image.
+        This is used to allow pixels to be non-monotonic up to
+        `variance` * `noise_rms`, so setting `variance = 0`
+        will force strict monotonicity in the mask.
+        Only used if `monotonicity` is `None`.
 
     Returns
     -------
@@ -111,24 +136,28 @@ def init_monotonic_morph(
     center: tuple[int, int] = tuple(center[i] - full_box.origin[i] for i in range(2))  # type: ignore
 
     if monotonicity is None:
-        _, morph, bounds = prox_monotonic_mask(detect, center, max_iter=0)
+        _, morph, bounds = prox_monotonic_mask(
+            x=detect,
+            center=center,
+            center_radius=center_radius,
+            variance=variance_factor,
+            max_iter=max_iter,
+        )
         bbox = bounds_to_bbox(bounds)
         if bbox.shape == (1, 1) and morph[bbox.slices][0, 0] == 0:
             return Box((0, 0)), None
 
         if thresh > 0:
-            morph, bbox = trim_morphology(morph, bg_thresh=thresh, padding=padding)
-
-        # Shift the bounding box to account for the non-zero origin
-        bbox += full_box.origin
+            morph, bbox = trim_morphology(morph, thresh=thresh, padding=padding)
 
     else:
         morph = monotonicity(detect, center)
 
         # truncate morph at thresh * bg_rms
-        morph, bbox = trim_morphology(morph, bg_thresh=thresh, padding=padding)
-        # Shift the bounding box to account for the non-zero origin
-        bbox += full_box.origin
+        morph, bbox = trim_morphology(morph, thresh=thresh, padding=padding)
+
+    # Shift the bounding box to account for the non-zero origin
+    bbox += full_box.origin
 
     if np.max(morph) == 0:
         return Box((0, 0), origin=full_box.origin), None
@@ -194,8 +223,15 @@ def multifit_spectra(
     return spectra
 
 
-class FactorizedInitialization(ABC):
+class FactorizedInitialization:
     """Common variables and methods for both Factorized Component schemes
+
+    There are a large number of parameters that are universal for all of the
+    sources being initialized from the same set of observed images.
+    To simplify the API those parameters are all initialized by this class
+    and passed to `init_source` for each source.
+    It also creates temporary objects that only need to be created once for
+    all of the sources in a blend.
 
     Parameters
     ----------
@@ -203,6 +239,8 @@ class FactorizedInitialization(ABC):
         The observation containing the blend
     centers:
         The center of each source to initialize.
+    detect:
+        The array that contains a 2D image used for detection.
     min_snr:
         The minimum SNR required per component.
         So a 2-component source requires at least `2*min_snr` while sources
@@ -212,20 +250,67 @@ class FactorizedInitialization(ABC):
         the component is initialized with only the
         monotonic pixels, otherwise the monotonicity operator is used to
         project the morphology to a monotonic solution.
+    disk_percentile:
+        The percentage of the overall flux to attribute to the disk.
+    bg_thresh:
+        The fraction of the background RMS to use as a threshold for the
+        morphology (in other words the threshold is set at
+        `bg_thresh * noise_rms`).
+    initital_bg_thresh:
+        The same as bg_thresh, but this allows a different background
+        threshold to be used for initialization.
+    thresh:
+        Deprecated. Use `initial_bg_thresh` instead.
+    padding:
+        The amount to pad the morphology to allow for extra flux
+        in the first few iterations before resizing.
     use_sparse_init:
         Use a monotonic mask to prevent initial source models from growing
         too large.
+    max_components:
+        The maximum number of components in the source.
+        This should be one or two.
+    convolved:
+        Deprecated. This is now calculated in __init__, but the
+        old API is supported until v28.0.
     """
-
     def __init__(
         self,
         observation: Observation,
-        convolved: Image,
         centers: Sequence[tuple[int, int]],
+        detect: np.ndarray | None = None,
         min_snr: float = 50,
         monotonicity: Monotonicity | None = None,
+        disk_percentile: float = 25,
+        initial_bg_thresh: float = 0.5,
+        bg_thresh: float = 0.25,
+        thresh: float | None = None,
+        padding: int = 2,
         use_sparse_init: bool = True,
+        max_components: int = 2,
+        convolved: Image | None = None,
     ):
+        if detect is None:
+            # Build the morphology detection image
+            detect = np.sum(
+                observation.images.data / (observation.noise_rms**2)[:, None, None],
+                axis=0,
+            )
+        self.detect = detect
+
+        if convolved is None:
+            _detect = Image(detect)
+            convolved = observation.convolve(_detect.repeat(observation.bands), mode="real")
+        else:
+            logger.warning(
+                "convolved is deprecated and will be removed in v28.0. "
+                "The convolved image is now calculated in __init__ and does "
+                "not need to be specified."
+            )
+
+        if thresh is not None:
+            initial_bg_thresh = thresh
+
         self.observation = observation
         self.convolved = convolved
         self.centers = centers
@@ -237,19 +322,39 @@ class FactorizedInitialization(ABC):
         # Convolve the PSF in order to set the spectrum
         # of a point source correctly.
         model_psf = Image(cast(np.ndarray, observation.model_psf)[0])
-        convolved = model_psf.repeat(observation.bands)
-        self.convolved_psf = observation.convolve(convolved, mode="real").data
+        convolved_psf = model_psf.repeat(observation.bands)
+        self.convolved_psf = observation.convolve(convolved_psf, mode="real").data
         # Get the "spectrum" of the PSF
         self.py = model_psf.shape[0] // 2
         self.px = model_psf.shape[1] // 2
         self.psf_spectrum = self.convolved_psf[:, self.py, self.px]
 
+        # Set the maximum number of components for any source in the blend
+        if max_components < 0 or max_components > 2:
+            raise ValueError(f"max_components must be 0, 1 or 2, got {max_components}")
+        self.max_components = max_components
+        self.initial_bg_thresh = initial_bg_thresh
+        self.bg_thresh = bg_thresh
+        self.padding = padding
+        self.disk_percentile = disk_percentile
+
         # Initalize all of the sources
         sources = []
         for center in centers:
-            source = self.init_source((int(center[0]), int(center[1])))
+            if max_components == 0:
+                source = Source([self.get_psf_component(center)])
+            else:
+                source = self.init_source((int(center[0]), int(center[1])))
             sources.append(source)
         self.sources = sources
+
+    @property
+    def thresh(self):
+        logger.warning(
+            "thresh is deprecated and will be removed in v28.0. "
+            "Use initial_bg_thresh instead."
+        )
+        return self.initial_bg_thresh
 
     def get_snr(self, center: tuple[int, int]) -> float:
         """Get the SNR at the center of a component
@@ -376,98 +481,15 @@ class FactorizedInitialization(ABC):
         morph /= morph_max
 
         return FactorizedComponent(
-            self.observation.bands,
-            spectrum,
-            morph,
-            bbox,
-            center,
-            self.observation.noise_rms,
+            bands=self.observation.bands,
+            spectrum=spectrum,
+            morph=morph,
+            bbox=bbox,
+            peak=center,
+            bg_rms=self.observation.noise_rms,
+            bg_thresh=self.bg_thresh,
             monotonicity=self.monotonicity,
         )
-
-    @abstractmethod
-    def init_source(self, center: tuple[int, int]) -> Source | None:
-        """Initialize a source
-
-        Parameters
-        ----------
-        center:
-            The center of the source.
-        """
-
-
-class FactorizedChi2Initialization(FactorizedInitialization):
-    """Initialize all sources with chi^2 detections
-
-    There are a large number of parameters that are universal for all of the
-    sources being initialized from the same set of observed images.
-    To simplify the API those parameters are all initialized by this class
-    and passed to `init_main_source` for each source.
-    It also creates temporary objects that only need to be created once for
-    all of the sources in a blend.
-
-    Parameters
-    ----------
-    observation:
-        The observation containing the blend
-    centers:
-        The center of each source to initialize.
-    detect:
-        The array that contains a 2D image used for detection.
-    min_snr:
-        The minimum SNR required per component.
-        So a 2-component source requires at least `2*min_snr` while sources
-        with SNR < `min_snr` will be initialized with the PSF.
-    monotonicity:
-        When `monotonicity` is `None`,
-        the component is initialized with only the
-        monotonic pixels, otherwise the monotonicity operator is used to
-        project the morphology to a monotonic solution.
-    disk_percentile:
-        The percentage of the overall flux to attribute to the disk.
-    thresh:
-        The threshold used to trim the morphology,
-        so all pixels below `thresh * bg_rms` are set to zero.
-    padding:
-        The amount to pad the morphology to allow for extra flux
-        in the first few iterations before resizing.
-    """
-
-    def __init__(
-        self,
-        observation: Observation,
-        centers: Sequence[tuple[int, int]],
-        detect: np.ndarray | None = None,
-        min_snr: float = 50,
-        monotonicity: Monotonicity | None = None,
-        disk_percentile: float = 25,
-        thresh: float = 0.5,
-        padding: int = 2,
-    ):
-        if detect is None:
-            # Build the morphology detection image
-            detect = np.sum(
-                observation.images.data / (observation.noise_rms**2)[:, None, None],
-                axis=0,
-            )
-        self.detect = detect
-        _detect = Image(detect)
-        # Convolve the detection image.
-        # This may seem counter-intuitive,
-        # since this is effectively growing the model,
-        # but this is exactly what convolution will do to the model
-        # in each iteration.
-        # So we create the convolved model in order
-        # to correctly set the spectrum.
-        convolved = observation.convolve(_detect.repeat(observation.bands), mode="real")
-
-        # Set the input parameters
-        self.disk_percentile = disk_percentile
-        self.thresh = thresh
-        self.padding = padding
-
-        # Initialize the sources
-        super().__init__(observation, convolved, centers, min_snr, monotonicity)
 
     def init_source(self, center: tuple[int, int]) -> Source | None:
         """Initialize a source from a chi^2 detection.
@@ -493,12 +515,12 @@ class FactorizedChi2Initialization(FactorizedInitialization):
         # Initialize the bbox, morph, and spectrum
         # for a single component source
         detect = prox_uncentered_symmetry(self.detect.copy(), local_center, fill=0)
-        thresh = np.mean(self.observation.noise_rms) * self.thresh
+        thresh = np.mean(self.observation.noise_rms) * self.initial_bg_thresh
         component = self.get_single_component(center, detect, thresh, self.padding)
 
         if component is None:
             components = [self.get_psf_component(center)]
-        elif component_snr < 2:
+        elif component_snr < 2 or self.max_components < 2:
             components = [component]
         else:
             # There was enough flux for a 2-component source,
@@ -537,21 +559,23 @@ class FactorizedChi2Initialization(FactorizedInitialization):
 
                 components = [
                     FactorizedComponent(
-                        self.observation.bands,
-                        bulge_spectrum,
-                        bulge_morph,
-                        component.bbox.copy(),
-                        center,
-                        self.observation.noise_rms,
+                        bands=self.observation.bands,
+                        spectrum=bulge_spectrum,
+                        morph=bulge_morph,
+                        bbox=component.bbox.copy(),
+                        peak=center,
+                        bg_rms=self.observation.noise_rms,
+                        bg_thresh=self.bg_thresh,
                         monotonicity=self.monotonicity,
                     ),
                     FactorizedComponent(
-                        self.observation.bands,
-                        disk_spectrum,
-                        disk_morph,
-                        component.bbox.copy(),
-                        center,
-                        self.observation.noise_rms,
+                        bands=self.observation.bands,
+                        spectrum=disk_spectrum,
+                        morph=disk_morph,
+                        bbox=component.bbox.copy(),
+                        peak=center,
+                        bg_rms=self.observation.noise_rms,
+                        bg_thresh=self.bg_thresh,
                         monotonicity=self.monotonicity,
                     ),
                 ]
@@ -561,6 +585,58 @@ class FactorizedChi2Initialization(FactorizedInitialization):
         return Source(components)  # type: ignore
 
 
+@deprecated(
+    reason="This class is replaced by FactorizedInitialization and will be removed in v28.0",
+    version="v28.0",
+    category=FutureWarning,
+)
+class FactorizedChi2Initialization(FactorizedInitialization):
+    """Initialize all sources with chi^2 detections
+
+    There are a large number of parameters that are universal for all of the
+    sources being initialized from the same set of observed images.
+    To simplify the API those parameters are all initialized by this class
+    and passed to `init_main_source` for each source.
+    It also creates temporary objects that only need to be created once for
+    all of the sources in a blend.
+
+    Parameters
+    ----------
+    observation:
+        The observation containing the blend
+    centers:
+        The center of each source to initialize.
+    detect:
+        The array that contains a 2D image used for detection.
+    min_snr:
+        The minimum SNR required per component.
+        So a 2-component source requires at least `2*min_snr` while sources
+        with SNR < `min_snr` will be initialized with the PSF.
+    monotonicity:
+        When `monotonicity` is `None`,
+        the component is initialized with only the
+        monotonic pixels, otherwise the monotonicity operator is used to
+        project the morphology to a monotonic solution.
+    disk_percentile:
+        The percentage of the overall flux to attribute to the disk.
+    thresh:
+        The threshold used to trim the morphology,
+        so all pixels below `thresh * bg_rms` are set to zero.
+    padding:
+        The amount to pad the morphology to allow for extra flux
+        in the first few iterations before resizing.
+    """
+    pass
+
+
+@deprecated(
+    reason="FactorizedWaveletInitialization will be removed in v28.0 "
+           "since it does not appear to offer any advantages over "
+           "FactorizedChi2Initialization. Consider switching to "
+           "FactorizedInitialization now.",
+    version="v28.0",
+    category=FutureWarning,
+)
 class FactorizedWaveletInitialization(FactorizedInitialization):
     """Parameters used to initialize all sources with wavelet detections
 
@@ -614,6 +690,7 @@ class FactorizedWaveletInitialization(FactorizedInitialization):
         wavelets: np.ndarray | None = None,
         monotonicity: Monotonicity | None = None,
         min_snr: float = 50,
+        use_sparse_init: bool = True,
     ):
         if wavelets is None:
             wavelets = get_detect_wavelets(
@@ -629,10 +706,6 @@ class FactorizedWaveletInitialization(FactorizedInitialization):
         # The detection coadd for the disk
         disklets = np.sum(wavelets[disk_slice], axis=0)
 
-        # The convolved image, used to initialize the spectrum
-        detect = Image(detectlets)
-        convolved = observation.convolve(detect.repeat(observation.bands), mode="real")
-
         self.detectlets = detectlets
         self.bulgelets = bulgelets
         self.disklets = disklets
@@ -641,7 +714,14 @@ class FactorizedWaveletInitialization(FactorizedInitialization):
         self.use_psf = use_psf
 
         # Initialize the sources
-        super().__init__(observation, convolved, centers, min_snr, monotonicity)
+        super().__init__(
+            observation=observation,
+            centers=centers,
+            detect=detectlets,
+            min_snr=min_snr,
+            monotonicity=monotonicity,
+            use_sparse_init=use_sparse_init,
+        )
 
     def init_source(self, center: tuple[int, int]) -> Source | None:
         """Initialize a source from a chi^2 detection.
